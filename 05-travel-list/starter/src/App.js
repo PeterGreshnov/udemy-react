216e441acbea6144af35916fec3bd2841c577669
Peter Greshnov <greshnov.peter@gmail.com>
2024-03-11 16:08:17 +0600

    S7L82 - Deleting item, lifting state up

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -1,18 +1,31 @@
 import { useState } from "react";
 
-const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: false },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-    { id: 3, description: "Charger", quantity: 1, packed: true },
-    { id: 4, description: "Toothbrush", quantity: 2, packed: false },
-];
+// const initialItems = [
+//     { id: 1, description: "Passports", quantity: 2, packed: false },
+//     { id: 2, description: "Socks", quantity: 12, packed: false },
+//     { id: 3, description: "Charger", quantity: 1, packed: true },
+//     { id: 4, description: "Toothbrush", quantity: 2, packed: false },
+// ];
 
 export default function App() {
+    const [items, setItem] = useState([]); // we might've used initialItems as initial state
+
+    function handleAddItems(item) {
+        setItem((items) => [...items, item]);
+    }
+
+    function handleDeleteItem(id) {
+        setItem((items) => items.filter((item) => item.id !== id));
+    }
+
     return (
         <div className="app">
             <Logo />
-            <Form />
-            <PackingList />
+            <Form onAddItems={handleAddItems} />
+            <PackingList
+                items={items}
+                onDeleteItem={handleDeleteItem}
+            />
             <Stats />
         </div>
     );
@@ -22,7 +35,7 @@ function Logo() {
     return <h1>🌴 Far Away 🏖️</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
     const [description, setDescription] = useState("");
     const [quantity, setQuantity] = useState(1);
 
@@ -40,8 +53,10 @@ function Form() {
         };
         console.log(newItem);
 
-        setQuantity(1);
+        onAddItems(newItem);
+
         setDescription("");
+        setQuantity(1);
     }
 
     return (
@@ -74,13 +89,14 @@ function Form() {
     );
 }
 
-function PackingList() {
+function PackingList({ items, onDeleteItem }) {
     return (
         <div className="list">
             <ul>
-                {initialItems.map((item) => (
+                {items.map((item) => (
                     <Item
                         item={item}
+                        onDeleteItem={onDeleteItem}
                         key={item.id}
                     />
                 ))}
@@ -89,13 +105,13 @@ function PackingList() {
     );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
     return (
         <li>
             <span style={item.packed ? { textDecoration: "line-through" } : {}}>
                 {item.quantity} {item.description}
             </span>
-            <button>❌</button>
+            <button onClick={() => onDeleteItem(item.id)}>❌</button>
         </li>
     );
 }
