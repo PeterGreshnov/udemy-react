b35c02c0269fb19e90e20a28079f8a85577e0755
Peter Greshnov <greshnov.peter@gmail.com>
2024-03-21 18:01:36 +0600

    S17L210 - Building layouts for WorldWise app

---
+++ 11-worldwise/starter/src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <h1>Application content</h1>
+    </div>
+  );
+}
+
+export default AppLayout;
