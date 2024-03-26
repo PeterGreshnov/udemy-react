f940beb0f8dfcd437dc10398cd986c6cb9be3aef
Peter Greshnov <greshnov.peter@gmail.com>
2024-03-26 22:22:02 +0600

    S17L217 - Reading and Setting URL Query Params

--- 11-worldwise/starter/src/components/Map.jsx
+++ 11-worldwise/starter/src/components/Map.jsx
@@ -1,7 +1,28 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>MAP</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  function handleClick() {
+    setSearchParams({ lat: "1", lng: "2", rng: "123" });
+  }
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>MAP</h1>
+      {lat && lng && (
+        <>
+          <h1>lat = {lat}</h1>
+          <h1>lng = {lng}</h1>
+        </>
+      )}
+      <button onClick={handleClick}>Change params</button>
+    </div>
+  );
 }
 
 export default Map;
