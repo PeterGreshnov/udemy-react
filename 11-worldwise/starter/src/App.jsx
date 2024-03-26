f0ac36eace586b5a53a0c232ef4cc6f8c9554b28
Peter Greshnov <greshnov.peter@gmail.com>
2024-03-26 22:08:20 +0600

    S17L216 - Dynamic routes, useParams()

--- 11-worldwise/starter/src/App.jsx
+++ 11-worldwise/starter/src/App.jsx
@@ -9,6 +9,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountriesList from "./components/CountriesList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -76,6 +77,7 @@ function App() {
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
             />
+            <Route path="cities/:id" element={<City />} />
             <Route
               path="countries"
               element={<CountriesList cities={cities} isLoading={isLoading} />}
