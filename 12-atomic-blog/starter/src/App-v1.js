1e41a8ab80725f97c34f7a5f898459c82f126fb4
Peter Greshnov <greshnov.peter@gmail.com>
2024-03-27 22:00:12 +0600

    S18L225 - Custom Provider & hook

--- 12-atomic-blog/starter/src/App-v1.js
+++ 12-atomic-blog/starter/src/App-v1.js
@@ -1,6 +1,5 @@
 import { useEffect, useState, createContext, useContext } from "react";
 import { faker } from "@faker-js/faker";
-import { PostProvider, usePost } from "./PostContext";
 
 function createRandomPost() {
   return {
@@ -9,10 +8,34 @@ function createRandomPost() {
   };
 }
 
-function App() {
-  // const x = usePost(); // that will throw an error, because we are not inside a <PostProvider> component.
+// 1. Create new context
+const PostContext = createContext();
 
+function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
     function () {
@@ -22,28 +45,38 @@ function App() {
   );
 
   return (
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
+    // 2. Provide context value. This is the value that will be available to all components that are descendants of this context provider.
 
-      <PostProvider>
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
+
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </PostProvider>
-    </section>
+      </section>
+    </PostContext.Provider>
   );
 }
 
 function Header() {
   // 3. CONSUMING: Use context value. This is the value that will be available to all components that are descendants of this context provider
 
-  const { onClearPosts } = usePost();
+  const { onClearPosts } = useContext(PostContext);
 
   return (
     <header>
@@ -60,7 +93,7 @@ function Header() {
 }
 
 function SearchPosts() {
-  const { searchQuery, setSearchQuery } = usePost();
+  const { searchQuery, setSearchQuery } = useContext(PostContext);
 
   return (
     <input
@@ -72,13 +105,13 @@ function SearchPosts() {
 }
 
 function Results() {
-  const { posts } = usePost();
+  const { posts } = useContext(PostContext);
 
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
 function Main() {
-  const { posts, onAddPost } = usePost();
+  const { posts, onAddPost } = useContext(PostContext);
 
   return (
     <main>
@@ -97,7 +130,7 @@ function Posts() {
 }
 
 function FormAddPost() {
-  const { onAddPost } = usePost();
+  const { onAddPost } = useContext(PostContext);
 
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
@@ -128,7 +161,7 @@ function FormAddPost() {
 }
 
 function List() {
-  const { posts } = usePost();
+  const { posts } = useContext(PostContext);
   return (
     <ul>
       {posts.map((post, i) => (
@@ -150,7 +183,7 @@ function Archive() {
 
   const [showArchive, setShowArchive] = useState(false);
 
-  const { onAddPost } = usePost();
+  const { onAddPost } = useContext(PostContext);
 
   return (
     <aside>
---
+++ 12-atomic-blog/starter/src/PostContext.js
@@ -0,0 +1,64 @@
+import { createContext, useContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+// 1. Create new context
+const PostContext = createContext();
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    // 2. Provide context value. This is the value that will be available to all components that are descendants of this context provider.
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+function usePost() {
+  const context = useContext(PostContext);
+
+  if (context === undefined) {
+    throw new Error("usePost must be used within a PostProvider");
+  }
+
+  return context;
+}
+
+export { PostProvider, PostContext, usePost };
